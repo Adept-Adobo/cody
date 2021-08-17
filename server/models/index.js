a762b62a1bcdfacda9163fd855a1d9694b41822b
Cody Szeto <codyszet@gmail.com>
2021-08-16 17:06:24 -0700

    Get reviews model function now returns data in the proper form

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,7 @@ const db = require('../../db/postgreSQL');
 module.exports = {
   get: (query) => {
     const {page, count, product_id, sort} = query;
-    return db.query(`SELECT id, product_id, rating, posting_date, summary, body, recommend, reported, response, helpfulness, reviewer_id FROM reviews WHERE product_id = ${product_id} ORDER BY id LIMIT ${count} OFFSET ${3 * (page - 1)}`);
+    return db.query(`SELECT id AS review_id, rating, summary, recommend, CASE response WHEN 'null' THEN null ELSE response END, body, posting_date AS date, (SELECT name FROM users WHERE id = reviews.reviewer_id LIMIT 1) AS reviewer_name, helpfulness, (SELECT CASE COUNT(photos) WHEN 0 THEN '[]' ELSE jsonb_agg(photos) END FROM (SELECT photos.id, photos.url FROM photos WHERE photos.review_id = reviews.id) AS photos) AS photos FROM reviews WHERE product_id = ${product_id} ORDER BY id LIMIT ${count} OFFSET ${3 * (page - 1)}`);
   },
   post: () => {console.log('post request')},
   getMeta: (query) => {
